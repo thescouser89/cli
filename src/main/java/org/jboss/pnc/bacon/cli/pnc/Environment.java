54963033819f38031bfec4898fad987e286b7014
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-01-04 17:33:51 -0500

    [WIP] Initial code to get client going

--- src/main/java/org/jboss/pnc/bacon/cli/pnc/Environment.java
+++ src/main/java/org/jboss/pnc/bacon/cli/pnc/Environment.java
@@ -1,6 +1,16 @@
 package org.jboss.pnc.bacon.cli.pnc;
 
+import org.jboss.pnc.dto.BuildEnvironment;
+import org.jboss.pnc.dto.response.Page;
+import org.jboss.pnc.rest.api.endpoints.EnvironmentEndpoint;
+import org.jboss.pnc.rest.api.parameters.PageParameters;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 import picocli.CommandLine;
+
+
 
 @CommandLine.Command(name = "environment", mixinStandardHelpOptions = true)
 public class Environment {
@@ -8,6 +18,19 @@ public class Environment {
     @CommandLine.Command(name = "get", mixinStandardHelpOptions = true)
     public void get() {
 
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        client.register(ResteasyJackson2Provider.class);
+        ResteasyWebTarget target = client.target("http://orch-master-devel.upshift.redhat.com/pnc-rest/rest");
+
+        EnvironmentEndpoint endpoint = target.proxy(EnvironmentEndpoint.class);
+
+        PageParameters params = new PageParameters();
+        params.setPageIndex(0);
+        params.setPageSize(200);
+
+        Page<BuildEnvironment> response = endpoint.getAll(params);
+
+        response.getContent().stream().forEach(a -> System.out.println(a.getDescription()));
     }
 
     @CommandLine.Command(name = "list", mixinStandardHelpOptions = true)
