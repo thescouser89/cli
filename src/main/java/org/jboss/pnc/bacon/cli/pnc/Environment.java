2013a7e4054c93d3c6d88b4e7c6ad55c6ebefcfc
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-02-01 19:58:25 -0500

    Update rest config

--- src/main/java/org/jboss/pnc/bacon/cli/pnc/Environment.java
+++ src/main/java/org/jboss/pnc/bacon/cli/pnc/Environment.java
@@ -1,6 +1,9 @@
 package org.jboss.pnc.bacon.cli.pnc;
 
 import org.jboss.pnc.bacon.cli.SubCommandHelper;
+import org.jboss.pnc.client.Configuration;
+import org.jboss.pnc.client.EnvironmentClient;
+import org.jboss.pnc.dto.BuildEnvironment;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "environment", mixinStandardHelpOptions = true)
@@ -8,6 +11,22 @@ public class Environment extends SubCommandHelper {
 
     @CommandLine.Command(name = "get", mixinStandardHelpOptions = true)
     public void get() {
+
+        Configuration connectionInfo = Configuration.builder()
+                .protocol("http")
+                .host("orch-master-devel.psi.redhat.com")
+                .pageSize(2)
+                .build();
+
+        EnvironmentClient client = new EnvironmentClient(connectionInfo);
+
+        try {
+            for (BuildEnvironment b: client.getAll()) {
+                System.out.println(b);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
